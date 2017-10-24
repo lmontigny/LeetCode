536e8888aa9cdaa8da6613066b51ab2611cb0415
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-24 23:13:12 +0200

    Create max-consecutive-ones.cpp

---
+++ max-consecutive-ones.cpp
@@ -0,0 +1,18 @@
+// 485. Max Consecutive Ones
+
+
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int count{0}, max{0};
+        for(int i=0; i<nums.size(); i++){
+            if(nums[i]==1) {
+                count++;
+                if(count>max) max = count;
+            }
+            else count = 0;
+        }
+        return max;
+        
+    }
+};
