25224b3c262a3ff52e65a35ca4afc0af54dd5a0f
Laurent Montigny <lmontigny@users.noreply.github.com>
2017-10-28 12:09:02 +0200

    Update plus-one.cpp

--- plus-one.cpp
+++ plus-one.cpp
@@ -5,8 +5,8 @@ public:
     vector<int> plusOne(vector<int> &digits) {
         bool carry = true;
         
-        for(int i=digits.size()-1; i >= 0 && carry; i--) {
-            carry = (++digits[i]%=10) == 0;
+        for(int i=digits.size()-1; i >= 0 && carry; i--) {   //keep going if carry is true
+            carry = (++digits[i]%=10) == 0;                  // 9->0
         }
 
         if(carry) {
